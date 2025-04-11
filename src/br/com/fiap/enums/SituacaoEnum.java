3227024dbfb5f54bdbb058c05a1b93ba594464df
justigliani <julianasilva.vania@gmail.com>
2025-04-11 06:00:24 -0300

    banco

---
+++ src/br/com/fiap/enums/SituacaoEnum.java
@@ -0,0 +1,6 @@
+package br.com.fiap.enums;
+
+public class SituacaoEnum {
+    ATIVA,
+    INATIVA
+}
---
+++ src/br/com/fiap/models/Cliente.java
@@ -0,0 +1,51 @@
+package br.com.fiap.models;
+
+public class Cliente {
+    private Long codigoCliente;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+
+    // Getters e Setters
+    public Long getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(Long codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
+
